e091fac4c0d423c6991250b19c5c4c26251df741
Ryan Endacott <rzeg24@gmail.com>
2012-12-02 19:20:06 -0600

    Added win conditions!  So it's a working tic tac toe.  It also should hopefully work with variable number of rows, columns, and variable win condition.  I haven't tested too many different variations though.

--- tictactoe.c
+++ tictactoe.c
@@ -5,8 +5,8 @@
 
 #include <stdio.h>
 
-#define NUMROWS 3
-#define NUMCOLS 3
+#define NUMROWS 4
+#define NUMCOLS 5
 #define WINCOND 3
 
 // The preprocessor will not multiply these two, HOWEVER, 
@@ -17,23 +17,27 @@
 #define TRUE 1
 #define FALSE 0
 
-
-// Function to see if someone has won
-int win(char board[NUMROWS][NUMCOLS]);
+// Function to see if someone has won after moving at position
+int win(char board[NUMROWS][NUMCOLS], int position);
 
 // Function to see if someone won horizontally
-// same return conditions as win
-int horiz(char board[NUMROWS][NUMCOLS]);
+// Row and col are latest move space, curVal is value to check against
+int horiz(char board[NUMROWS][NUMCOLS], int row, int col, char curVal);
 
 
 // Function to see if someone won vertically
-// same return conditions as win
-int vert(char board[NUMROWS][NUMCOLS]);
+// Row and col are latest move space, curVal is value to check against
+int vert(char board[NUMROWS][NUMCOLS], int row, int col, char curVal);
 
 
-// Function to see if someone won diagonally
-// same return conditions as win
-int diag(char board[NUMROWS][NUMCOLS]);
+// Function to see if someone won positive diagonally
+// Row and col are latest move space, curVal is value to check against
+int posDiag(char board[NUMROWS][NUMCOLS], int row, int col, char curVal);
+
+
+// Function to see if someone won negative diagonally
+// Row and col are latest move space, curVal is value to check against
+int negDiag(char board[NUMROWS][NUMCOLS], int row, int col, char curVal);
 
 
 // Prints the board
@@ -46,11 +50,20 @@ void reset(char board[NUMROWS][NUMCOLS]);
 
 
 // Lets the current player have a turn
-void playerTurn(char board[NUMROWS][NUMCOLS], int player);
+// Returns position of latest turn
+int playerTurn(char board[NUMROWS][NUMCOLS], int player);
 
 
 // Method to get a valid space to move to
 int getValidSpace(char board[NUMROWS][NUMCOLS]);
+
+
+// Returns min
+int min (int x, int y);
+
+// Returns max
+int max (int x, int y);
+
 
 
 int main(void)
@@ -76,14 +89,13 @@ int main(void)
     {
 
         // Player has a turn
-        playerTurn(board, player);
+        int position = playerTurn(board, player);
 
 
-        if (win(board)) 
+        if (win(board, position)) 
         {
             winner = player;
         }
-        printf("player: %d\n", player);
 
         player = (player == 1) ? 2 : 1;
 
@@ -133,20 +145,183 @@ void reset(char board[NUMROWS][NUMCOLS])
     }
 }
 
-int win(char board[NUMROWS][NUMCOLS])
+// Function to see if someone has won after moving at position
+int win(char board[NUMROWS][NUMCOLS], int position)
 {
-    return 0;    
+
+    // Zero-indexed
+    position--;
+
+    // board[row][col] = row * NUMCOLS + col
+    int row = position / NUMCOLS;
+    int col = position % NUMCOLS;
+
+
+    // Value to check against to see if won
+    char curVal = board[row][col];
+
+
+    return horiz(board, row, col, curVal) ||
+        vert(board, row, col, curVal) || 
+        posDiag(board, row, col, curVal) ||
+        negDiag(board, row, col, curVal);
 }
 
-int horiz(char board[NUMROWS][NUMCOLS])
+// Function to see if someone won horizontally
+// Row and col are latest move space, curVal is value to check against
+int horiz(char board[NUMROWS][NUMCOLS], int row, int col, char curVal)
 {
+
+    // Only need to check within range of a possible win,
+    // Also must be in bounds
+    int endCol = min(col + WINCOND - 1, NUMCOLS - 1);
+    col = max(col - WINCOND + 1, 0);
+
+
+    int count = 0;
+    for (;col <= endCol; col++)
+    {
+        if (board[row][col] == curVal)
+            count++;
+        else
+            count = 0;
+
+        if (count == WINCOND)
+            return 1;
+
+    }
+
+    return 0;
+
+}
+
+// Function to see if someone won vertically
+// Row and col are latest move space, curVal is value to check against
+int vert(char board[NUMROWS][NUMCOLS], int row, int col, char curVal)
+{
+
+
+    // Only need to check within range of a possible win,
+    // Also must be in bounds
+    int endRow = min(row + WINCOND - 1, NUMROWS - 1);
+    row = max(row - WINCOND + 1, 0);
+
+
+    int count = 0;
+    for (;row <= endRow; row++)
+    {
+        if (board[row][col] == curVal)
+            count++;
+        else
+            count = 0;
+
+        if (count == WINCOND)
+            return 1;
+
+    }
+
+    return 0;
+
+}
+
+// Function to see if someone won positive diagonally
+// Row and col are latest move space, curVal is value to check against
+int posDiag(char board[NUMROWS][NUMCOLS], int row, int col, char curVal)
+{
+
+    // Set up endRow and endCol by setting to both to max possible
+    int endRow = min(row + WINCOND - 1, NUMROWS - 1);
+    int endCol = min(col + WINCOND - 1, NUMCOLS - 1);
+
+    // Then equalizing them to the max in bounds and on diag
+    int diff = min(endRow - row, endCol - col); 
+    endRow = row + diff;
+    endCol = col + diff;
+
+    // Set up row and col the same way, both to min possible
+    row = max(row - WINCOND + 1, 0);
+    col = max(col - WINCOND + 1, 0);
+
+    // Then equalize to min in bounds and on diag
+    diff = min(endRow - row, endCol - col);
+    row = endRow - diff;
+    col = endCol - diff;
+
+
+
+    int count = 0;
+
+    // Because all are equalized, only one counter (row to endRow) should be needed
+    while (row <= endRow)
+    {
+        if (board[row][col] == curVal)
+            count++;
+        else
+            count = 0;
+
+        if (count == WINCOND)
+            return 1;
+
+        row++;
+        col++;
+    }
+
     return 0;
 }
 
 
+// Function to see if someone won negative diagonally
+// Row and col are latest move space, curVal is value to check against
+int negDiag(char board[NUMROWS][NUMCOLS], int row, int col, char curVal)
+{
+   // Set up endRow and endCol by setting one to max, other to min
+    int endRow = min(row + WINCOND - 1, NUMROWS - 1);
+    int endCol = max(col - WINCOND + 1, 0);
+
+    // Then equalizing them to the values such that they will both be in bounds and on the diag
+    int diff = min(endRow - row, col - endCol);
+    endRow = row + diff;
+    endCol = col - diff;
+
+    // Set up row and col the same way, both to best possible
+    row = max(row - WINCOND + 1, 0);
+    col = min(col + WINCOND - 1, NUMCOLS - 1);
+
+    // Then equalize to best in bounds and on diag
+    diff = min(endRow - row, col - endCol);
+    row = endRow - diff;
+    col = endCol + diff;
+
+
+
+    int count = 0;
+
+    // Because all are equalized, only one counter (row to endRow) should be needed
+    while (row <= endRow)
+    {
+        if (board[row][col] == curVal)
+            count++;
+        else
+            count = 0;
+
+        if (count == WINCOND)
+            return 1;
+
+        row++;
+        col--;
+    }
+
+    return 0;
+
+
+
+    return 0;
+}
+
 
 // Lets the current player have a turn
-void playerTurn(char board[NUMROWS][NUMCOLS], int player)
+// Returns position of latest turn
+int playerTurn(char board[NUMROWS][NUMCOLS], int player)
 {
 
     int input;
@@ -193,6 +368,8 @@ void playerTurn(char board[NUMROWS][NUMCOLS], int player)
     }
 
     count++;  
+
+    return input;
 }
 
 // Method to get a valid space to move to
@@ -223,4 +400,15 @@ int getValidSpace(char board[NUMROWS][NUMCOLS])
 }
 
 
+// Returns min
+int min (int x, int y)
+{
+    return (x > y) ? y : x;
+}
+
+// Returns max
+int max (int x, int y)
+{
+    return (x > y) ? x: y;
+}
 
